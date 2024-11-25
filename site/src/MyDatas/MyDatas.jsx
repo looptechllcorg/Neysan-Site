32b448f1da9473fedf19e6deb148daabb1097cbb
Sadiqovilkin <sadiqovilkin068@gmail.com>
2024-11-25 12:30:19 +0400

    #Product page & data filter finished

--- site/src/MyDatas/MyDatas.jsx
+++ site/src/MyDatas/MyDatas.jsx
@@ -58,6 +58,7 @@ export const homeSliderData =[
     },
 ]
 
+// HomeMedia PlayVideo
 import videoSliderImgOne from "../assets/image/BgMedia1.png"
 import videoSliderImgTwo from "../assets/image/homeVideoSliderBgImg.png"
 
@@ -79,3 +80,89 @@ export const homeVideoSliderSection = [
     // },
 ];
 
+// Products
+import productTomat from "../assets/image/productImage/productImage-Tomat.png"
+import productAggilas from "../assets/image/productImage/productImage-AgGilas.png"
+import productGavali from "../assets/image/productImage/productImage-Gavali.png"
+import productMorug from "../assets/image/productImage/productImage-Morug.png"
+import productQaragat from "../assets/image/productImage/productImage-Qaragat.png"
+import productSaftali from "../assets/image/productImage/productImage-Saftali.png"
+import productYarpaq from "../assets/image/productImage/productImage-Yarpaq.png"
+import productZeytun from "../assets/image/productImage/productImage-Zeytun.png"
+
+export const productsData = [
+    {
+        id:1,
+        bgColor:"#BB1B23",
+        image:productTomat,
+        productName:"Pomidor Qurusu Zeytun yaginda",
+        productWeight:"320qr",
+        category: "Vegetable",
+    },
+    {
+        id:2,
+        bgColor:"#C99500",
+        image:productAggilas,
+        productName:"Ag Gilas murebbesi Limon Ile",
+        productWeight:"320qr",
+        category: "Fruit",
+    },
+    {
+        id:3,
+        bgColor:"#312D45",
+        image:productQaragat,
+        productName:"Qara Qaragat murebbesi",
+        productWeight:"320qr",
+        category: "Fruit",
+    },
+    {
+        id:4,
+        bgColor:"#AA7BAE",
+        image:productGavali,
+        productName:"Gavali Cemi",
+        productWeight:"320qr",
+        category: "Fruit",
+
+    },
+    {
+        id:5,
+        bgColor:"#6A232C",
+        image:productMorug,
+        productName:"Morug murebbesi",
+        productWeight:"320qr",
+        category: "Fruit",
+    },
+    {
+        id:6,
+        bgColor:"#56722F",
+        image:productZeytun,
+        productName:"Zeytun murebbesi Limon ile",
+        productWeight:"320qr",
+        category: "Vegetable",
+    },
+    {
+        id:7,
+        bgColor:"#A5E460",
+        image:productYarpaq,
+        productName:"Uzum Yarpagi",
+        productWeight:"320qr",
+        category: "Vegetable",
+    },
+    {
+        id:8,
+        bgColor:"#C8311F",
+        image:productSaftali,
+        productName:"Saftali Murebbesi",
+        productWeight:"320qr",
+        category: "Fruit",
+    },
+    {
+        id:9,
+        bgColor:"#D9C4E4",
+        image:productGavali,
+        productName:"Zeferan",
+        productWeight:"320qr",
+        category: "Vegetable",
+    },
+]
+export const categories = ["All", "Fruit", "Vegetable"];
--- site/src/pages/client/product/Product.jsx
+++ site/src/pages/client/product/Product.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./Product.scss"
+import productCloud from "../../../assets/image/productImage/productCloud.png"
+import { categories, productsData } from '../../../MyDatas/MyDatas'
+import { motion, AnimatePresence } from "framer-motion";
 const Product = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+  const filteredProducts = selectedCategory === "All"
+    ? productsData 
+    : productsData.filter(product => product.category === selectedCategory);
   return (
-    <div>Product</div>
+    <main>
+      <section id='products'>
+          <div className="container">
+            <div className="row">
+                  <div className="sectionHeader">
+                    <p>.. Increased HEALTH With NEYSAN ..</p>
+                    <h2>PRODUCTS</h2>
+                    <h3>TƏBİƏTDƏN SİZƏ TİPLİ TEXT</h3>
+                  </div>
+            </div>
+            <div className="row py-3">
+            {categories.map((category) => (
+             <div className="col-2">
+               <button
+                key={category}
+                onClick={() => setSelectedCategory(category)}
+                className={`filterBtn ${
+                  selectedCategory === category ? "btnActive" : ""
+                }`}
+              >
+                {category}
+              </button>
+             </div>
+            ))}
+
+          </div>
+            <div className="row py-5">
+            <AnimatePresence>
+            {productsData && filteredProducts.map((product , index)=>(
+                   <motion.div
+                   key={product.id}
+                   className="col-xxl-3 col-lg-4 col-md-6 col-sm-12"
+                   initial={{ opacity: 0, y: 20 }}
+                   animate={{ opacity: 1, y: 0 }}
+                   exit={{ opacity: 0, y: -20 }} 
+                   transition={{ duration: 0.4 }} 
+                 >
+
+                <div className="productCard">
+                  <div className="cardImg" style={{ "--circle-color": `${product.bgColor}` }}>
+                    <img src={product.image} alt={product.productName} />
+                    <div className="imageBackground"></div>
+                   <img className='cloud' src={productCloud} alt="" />
+                  </div>
+                  <div className="cardText">
+                    <h4>{product.productName}</h4>
+                    <p> {product.productWeight}</p>
+                  </div>
+                </div>
+              
+              </motion.div>
+              )
+                
+              )}
+            </AnimatePresence>
+             
+              
+            </div>
+          </div>
+      </section>
+    </main>
   )
 }
 
