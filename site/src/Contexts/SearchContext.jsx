2f4f96054f9c8501aac248ed8a91e27bcb87c176
Sadiqovilkin <sadiqovilkin068@gmail.com>
2025-01-01 23:30:01 +0400

    #search Finished

--- site/src/Contexts/SearchContext.jsx
+++ site/src/Contexts/SearchContext.jsx
@@ -1,11 +1,27 @@
-import {  createContext, useContext } from "react";
+import {  createContext, useContext, useState } from "react";
+import { productsData } from "../MyDatas/MyDatas";
 
 const dataContext = createContext(null)
 
 const DataContextProvider = ({children})=>{
     const [searchInputValue, setSearchInputValue]=useState("")
+    const [filteredItems, setFilteredItems]=useState([])
 
-    const values = {searchInputValue,setSearchInputValue};
+    // console.log("Products DATA", productsData);
+    const searchFunctions = ()=>{
+        // console.log("searchVAlue" , searchInputValue.toLocaleLowerCase());
+        
+       setFilteredItems( productsData.filter((item)=>{
+        // console.log("Items Name",item.productName.toLocaleLowerCase());
+        
+        return item.productName.toLocaleLowerCase().includes(searchInputValue.toLocaleLowerCase())
+    }))
+    // console.log("Filtered ITems",filteredItems);
+    
+    }
+    
+
+    const values = {searchInputValue,setSearchInputValue,filteredItems,searchFunctions};
     return <dataContext.Provider value={values}>{children}</dataContext.Provider>;
 }
 
