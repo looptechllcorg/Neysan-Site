6aa905a588f6a22089a8fdd8b26cc6282c77e21f
Sadiqovilkin <sadiqovilkin068@gmail.com>
2025-01-28 14:17:30 +0400

    #Some animation added

--- src/pages/client/home/homeInformation/HomeInformation.jsx
+++ src/pages/client/home/homeInformation/HomeInformation.jsx
@@ -1,14 +1,80 @@
 import React, { useEffect, useState } from 'react'
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import "./HomeInformation.scss"
 const HomeInformation = ({sectionData}) => {
+  const words = sectionData.info.split(" ");
+  const title = sectionData.title.split(" ");
+  const { ref, inView } = useInView({
+    triggerOnce: false, 
+    threshold: 0.3, 
+  });
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const child = {
+    hidden: { opacity: 0, y: 20 }, 
+    visible: { opacity: 1, y: 0 }, 
+  };
+
 
   return (   
-    <section id='homeInformation' >
+    <section ref={ref} id='homeInformation' >
         <div className="container-lg ">
             <div className="row">
             <div className="informationBox col-lg-4 col-sm-4" >
-            <h3>{sectionData.title}</h3>
-            <p>{sectionData.info}</p>
+            <motion.div
+      variants={container}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      style={{
+        display: "flex",
+        gap: "4px",
+        justifyContent: "start",
+        flexWrap: "wrap",
+      }}
+    >
+      {title.map((word, index) => (
+        <motion.span
+          key={index}
+          variants={child}
+          className='motionHeader'
+         
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.div>
+            <motion.div
+      variants={container}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      style={{
+        display: "flex",
+        gap: "4px",
+        justifyContent: "start",
+        flexWrap: "wrap",
+      }}
+    >
+      {words.map((word, index) => (
+        <motion.span
+          key={index}
+          variants={child}
+          className='motionSpan'
+         
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.div>
              </div>
             </div>
         <div className="bgImage" style={{backgroundImage:`url(${sectionData.bgImg})`}}></div>
