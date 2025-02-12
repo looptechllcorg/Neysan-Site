c4ca05b9fe320a2ca6b1ef6d384c3a872c562f86
shahmarrzayev <shahmar.rza@gmail.com>
2025-02-12 22:51:41 +0400

    bottle sizes changed

--- src/pages/client/ClientRoot.jsx
+++ src/pages/client/ClientRoot.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
-import ScrollToTop from '../../components/ScrolToTop/ScrolToTop'
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import ScrollToTop from '../../components/ScrolToTop/ScrolToTop';
 
 const ClientRoot = () => {
-  return (
-    <>
-    <Header/>
-    <Outlet />
-    <Footer/>
-    <ScrollToTop/>
-    </>
-  )
-}
+	const location = useLocation();
+	if (location.pathname === '/') {
+		window.disableScroll();
+	} else window.enableScroll();
+	return (
+		<>
+			<Header />
+			<Outlet />
+			<Footer />
+			<ScrollToTop />
+		</>
+	);
+};
 
-export default ClientRoot
+export default ClientRoot;
